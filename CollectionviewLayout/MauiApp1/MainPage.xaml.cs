89041cdedfaccc94e2fec8efb4acc28d43b54d00
Uridel <lion.drie@gmail.com>
2024-05-01 13:03:35 +0200

    Initial commit

---
+++ CollectionviewLayout/MauiApp1/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿
+namespace MauiApp1
+{
+	public partial class MainPage : ContentPage
+	{
+		public IList<TestItem> Items { get; set; }
+
+		public MainPage()
+		{
+			BindingContext = this;
+			InitializeComponent();
+			Items = new List<TestItem>();
+			Items.Add(new TestItem() { Name = "testItem1" });
+			//Items.Add(new TestItem() { Name = "testItem2"});
+			collection.ItemsSource = Items;
+
+		}
+
+		public class TestItem
+		{
+			public string? Name { get; set; }
+		}
+	}
+}
